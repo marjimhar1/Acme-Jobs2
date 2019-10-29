79f9821b033b15addc5fb76d7f7c227c4a89249b
carlos santos <carsantir@alum.us.es>
2019-10-29 22:00:44 +0100

    Task 006:finished

--- src/main/java/acme/features/anonymous/announcement/AnonymousAnnouncementRepository.java
+++ src/main/java/acme/features/anonymous/announcement/AnonymousAnnouncementRepository.java
@@ -15,6 +15,6 @@ public interface AnonymousAnnouncementRepository extends AbstractRepository {
 	@Query("select s from Announcement s where s.id=?1")
 	Announcement findOneById(int id);
 
-	@Query("select s from Announcement s where EXTRACT(MONTH FROM sysdate())-EXTRACT(MONTH FROM moment)<=ABS(1) and EXTRACT(YEAR FROM sysdate())=EXTRACT(YEAR FROM moment)")
+	@Query("select s from Announcement s where (ABS(EXTRACT(MONTH FROM sysdate())-EXTRACT(MONTH FROM moment))<=1 and EXTRACT(YEAR FROM sysdate())=EXTRACT(YEAR FROM moment)) or (EXTRACT(YEAR FROM sysdate())-EXTRACT(YEAR FROM moment)<=1 and EXTRACT(MONTH FROM sysdate())-EXTRACT(MONTH FROM moment)=-11)")
 	Collection<Announcement> findManyAll();
 }
