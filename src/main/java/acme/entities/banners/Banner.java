a0d81def58b07366edad6387d3260db1e67070e4
Mario David <marjimhar@alum.us.es>
2019-11-06 22:17:29 +0100

    Project: finished

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -4,6 +4,8 @@ package acme.entities.banners;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.URL;
+
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,12 +18,14 @@ public abstract class Banner extends DomainEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
+	@URL
 	private String				pictureUrl;
 
 	@NotBlank
 	private String				slogan;
 
 	@NotBlank
+	@URL
 	private String				targetUrl;
 
 }
